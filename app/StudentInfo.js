d17bd604cc7c2257cc2d72047c43bcef2337e2e8
Justine <119976731+Dabudash@users.noreply.github.com>
2023-09-16 12:36:22 -0600

    assignment

---
+++ app/StudentInfo.js
@@ -0,0 +1,9 @@
+export default function StudentInfo() {
+    return (
+      <div>
+        Name: Justine Barredo<br></br>
+        Course Section: CPRG306-F<br></br>
+        
+      </div>
+    );
+  }
