50ceb3d0e7b6f3b8377029b4ee8aa493584d67a6
Justine <119976731+Dabudash@users.noreply.github.com>
2023-09-26 14:07:27 -0600

    try

---
+++ app/week3/page.js
@@ -0,0 +1,15 @@
+import ItemList from "./item-list";
+
+export default function Page() {
+    return(
+        <main>
+            <h1>My Shopping List </h1>
+            <p></p>
+            <ItemList />
+           
+        </main>
+    );
+    
+
+
+}
